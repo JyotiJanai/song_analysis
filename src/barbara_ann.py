63b5724a6695472fde6bb700372212b856e3276c
Jyoti Janai <jyoti.janai10@gmail.com>
2020-09-12 19:34:47 -0500

    bar graph added

---
+++ src/barbara_ann.py
@@ -0,0 +1,17 @@
+from matplotlib import pyplot as plt
+
+lyrics = "Ah, Ba Ba Ba Ba Barbara Ann Ba Ba Ba Ba Barbara Ann Oh Barbara Ann Take My Hand Barbara Ann You Got Me Rockin' And A-Rollin' Rockin' And A-Reelin' Barbara Ann Ba Ba Ba Barbara Ann ...More Lyrics... Ba Ba Ba Ba Barbara Ann Ba Ba Ba Ba Barbara Ann"
+list_of_lyrics = lyrics.split(' ')
+print(len(list_of_lyrics))
+unique_words = set(list_of_lyrics)
+print(len(unique_words))
+word_histogram = dict.fromkeys(unique_words, 0)
+for word in list_of_lyrics:
+    word_histogram[word] = word_histogram[word]+ 1 
+print(word_histogram)
+
+print(list(unique_words))
+fig = plt.figure()
+ax = fig.add_axes([0,0,1,1])
+ax.bar(list(unique_words),list(word_histogram.values()))
+plt.show()
