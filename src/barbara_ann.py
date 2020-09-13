124be85a4b190869247d8d1754258ca8bd5ec57e
Jyoti Janai <jyoti.janai10@gmail.com>
2020-09-12 20:13:44 -0500

    code added

--- src/barbara_ann.py
+++ src/barbara_ann.py
@@ -1,4 +1,20 @@
 from matplotlib import pyplot as plt
+# import numpy
+# word_histogram = dict.fromkeys(unique_words, 0)
+# for word in list_of_lyrics:
+#     word_histogram[word] = word_histogram[word]+ 1 
+# print(word_histogram)
+
+# print(list(unique_words))
+# fig = plt.figure()
+# ax = fig.add_axes([0,0,1,1])
+# ax.bar(list(unique_words),list(word_histogram.values()))
+# plt.bar(list(unique_words), list(word_histogram.values()), color ='maroon',  
+#         width = 0.4)
+# plt.xlabel("Unique song words")
+# plt.ylabel("No. of words")
+# plt.title("Barbara ann song repeated words graph")
+# plt.show()
 
 lyrics = "Ah, Ba Ba Ba Ba Barbara Ann Ba Ba Ba Ba Barbara Ann Oh Barbara Ann Take My Hand Barbara Ann You Got Me Rockin' And A-Rollin' Rockin' And A-Reelin' Barbara Ann Ba Ba Ba Barbara Ann ...More Lyrics... Ba Ba Ba Ba Barbara Ann Ba Ba Ba Ba Barbara Ann"
 list_of_lyrics = lyrics.split(' ')
@@ -20,3 +36,11 @@ plt.xlabel("Unique song words")
 plt.ylabel("No. of words")
 plt.title("Barbara ann song repeated words graph")
 plt.show()
+
+print("Bar graph created")
+print("bar garph created")
+
+# plt.xlabel("Unique song words")
+# plt.ylabel("No. of words")
+# plt.title("Barbara ann song repeated words graph")
+# plt.show()
